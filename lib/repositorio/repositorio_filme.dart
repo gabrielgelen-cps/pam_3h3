f19af3b5e0321947ed067bd5143a0a212bbd4df2
gabrielgelen-cps <gabriel.gelen@etec.sp.gov.br>
2024-08-20 22:54:13 -0300

    branch com o código da aula do dia Tue, Aug 20, 2024 10:54:13 PM

---
+++ lib/repositorio/repositorio_filme.dart
@@ -0,0 +1,40 @@
+import 'package:absolute_cinema/entidade/filme.dart';
+
+class RepositorioFilme {
+  List<Filme> select(String id) {
+    if (id == "1") {
+      return <Filme>[
+        Filme(
+          titulo: "Inside Out",
+          poster:
+              "https://http2.mlstatic.com/D_NQ_NP_970881-MLB25924461827_082017-O.webp",
+          sinopse:
+              "Inside Out follows the inner workings of the mind of Riley, a young girl who adapts to her family's relocation as five personified emotions administer her thoughts and actions",
+          duracao: 105,
+          classificacao: "Livre",
+        ),
+        Filme(
+          titulo: "Ratatouille",
+          poster:
+              "https://m.media-amazon.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_FMjpg_UX1000_.jpg",
+          sinopse:
+              "Remy lives in Paris and has a sophisticated palate. His dream is to become a chef and enjoy the various works of culinary art. The only problem is that he's a rat. When he finds himself inside one of the finest restaurants in Paris, Remy decides to make his dream a reality.",
+          duracao: 111,
+          classificacao: "Livre",
+        )
+      ];
+    }
+
+    return <Filme>[
+      Filme(
+        titulo: "Inside Out",
+        poster:
+            "https://http2.mlstatic.com/D_NQ_NP_970881-MLB25924461827_082017-O.webp",
+        sinopse:
+            "Inside Out follows the inner workings of the mind of Riley, a young girl who adapts to her family's relocation as five personified emotions administer her thoughts and actions",
+        duracao: 105,
+        classificacao: "Livre",
+      ),
+    ];
+  }
+}
